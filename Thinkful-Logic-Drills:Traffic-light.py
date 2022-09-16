dba76f4435c3a2f8e972140dee36e59b8fba84c0
Karola-ING <108148869+Karola-ING@users.noreply.github.com>
2022-09-16 23:11:36 +0200

    Kyu 8

---
+++ Thinkful-Logic-Drills:Traffic-light.py
@@ -0,0 +1,10 @@
+# https://www.codewars.com/kata/58649884a1659ed6cb000072
+
+def update_light(current):
+    while True:
+        if current == 'green':
+            return 'yellow'
+        elif current == 'yellow':
+            return 'red'
+        elif current == 'red':
+            return 'green'
