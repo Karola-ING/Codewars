ad54c40c8b22b0b3335d1d494f39e920b207d039
Karola-ING <108148869+Karola-ING@users.noreply.github.com>
2022-09-17 01:16:47 +0200

    Kyu 7

---
+++ Shortest-word.py
@@ -0,0 +1,21 @@
+def number_list(s):
+    word_list = list(s.split(" "))
+
+    number_list = []
+    for element in word_list:
+        number = 0
+        for letter in element:
+            number += 1 
+        number_list.append(number)
+        
+    return number_list
+
+
+def find_short(s):
+    final_list = number_list(s)
+    l = min(final_list)
+        
+    return l
+
+print(find_short('bitcoin take'))
+print(find_short('bitcoin take over the world maybe who knows perhaps'))
